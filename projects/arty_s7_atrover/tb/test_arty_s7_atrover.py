029e59feb6109c5177c80de2cc87f0f10fee2197
Danilo Ramos <dramoz@gmail.com>
2022-07-20 19:15:05 -0400

    GitHub report test with implementation from Vivado

--- projects/arty_s7_atrover/tb/test_arty_s7_atrover.py
+++ projects/arty_s7_atrover/tb/test_arty_s7_atrover.py
@@ -11,26 +11,28 @@ import cocotb
 from cocotb.clock import Clock
 from cocotb.triggers import Timer, RisingEdge
 
-CLK_FREQ = 12e6
+CLK_FREQ = 100e6
 
 async def init(dut, clk_period, units):
-  dut.rst.value = 0
-  dut.clk.value = 0
-  dut.sw.value  = 0
-  dut.btn.value = 0
+  dut.resetn.value = 0
+  dut.clk.value    = 0
+  
+  dut.sw.value      = 0
+  dut.btn.value     = 0
+  dut.uart_rx.value = 0
   
   cocotb.start_soon(Clock(signal=dut.clk, period=clk_period, units="ns").start())
   
 
 async def reset(dut):
-  dut.rst.value = 0
+  dut.resetn.value = 0
   for _ in range(5):
     await RisingEdge(dut.clk)
     
-  dut.rst.value = 1
+  dut.resetn.value = 1
 
 @cocotb.test()
-async def free_run_arty_s7_atrover(dut, times=100, duration=1, units='ms'):
+async def free_run_arty_s7_atrover(dut, times=10, duration=1, units='us'):
   """Just let it run a few microseconds"""
   
   clk_period = int(1e9/CLK_FREQ)
